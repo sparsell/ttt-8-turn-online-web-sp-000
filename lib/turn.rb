5029c02a3cf8e97126e6cd774232bb5ca704a5b1
Sonja Parsell <hello@sparsell.com>
2020-03-11 16:26:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board(board)
+#display_board(board)
 
 def input_to_index(input)
   index = input.to_i - 1
