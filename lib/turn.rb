4a6f8758e4b40170f053b6725b487396983f37ca
Sonja Parsell <hello@sparsell.com>
2020-03-11 16:25:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,3 @@
-#ask for input
-#get input
-#convert input to index
-#if index is valid
-#  make the move for index
-#  show the board
-#else
-#  ask for input again until you get a valid input
-#end
-
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
